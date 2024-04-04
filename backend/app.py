64b77b64539e144594e45c6b36bf67ddc7530593
wally_yoon <bluegmlduf2@naver.com>
2024-04-04 21:47:03 +0900

    fix : 불필요한부분삭제

--- backend/app.py
+++ backend/app.py
@@ -8,48 +8,12 @@ import ast
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 
-
-BOOKS = [
-    {
-        "id": uuid.uuid4().hex,
-        "title": "On the Road",
-        "author": "Jack Kerouac",
-        "read": True,
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "title": "Harry Potter and the Philosopher's Stone",
-        "author": "J. K. Rowling",
-        "read": False,
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "title": "Green Eggs and Ham",
-        "author": "Dr. Seuss",
-        "read": True,
-    },
-]
-
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-
-def remove_book(book_id):
-    for book in BOOKS:
-        if book["id"] == book_id:
-            BOOKS.remove(book)
-            return True
-    return False
-
-
-# sanity check route
-@app.route("/ping", methods=["GET"])
-def ping_pong():
-    return jsonify("pong!")
 
 
 @app.route("/caption", methods=["GET"])
@@ -92,55 +56,15 @@ def get_caption():
     )
 
 
-@app.route("/books", methods=["GET", "POST"])
-def all_books():
-    response_object = {"status": "success"}
-    if request.method == "POST":
-        post_data = request.get_json()
-        BOOKS.append(
-            {
-                "id": uuid.uuid4().hex,
-                "title": post_data.get("title"),
-                "author": post_data.get("author"),
-                "read": post_data.get("read"),
-            }
-        )
-        response_object["message"] = "Book added!"
-    else:
-        response_object["books"] = BOOKS
-    return jsonify(response_object)
-
-
-@app.route("/books/<book_id>", methods=["PUT", "DELETE"])
-def single_book(book_id):
-    response_object = {"status": "success"}
-    if request.method == "PUT":
-        post_data = request.get_json()
-        remove_book(book_id)
-        BOOKS.append(
-            {
-                "id": uuid.uuid4().hex,
-                "title": post_data.get("title"),
-                "author": post_data.get("author"),
-                "read": post_data.get("read"),
-            }
-        )
-        response_object["message"] = "Book updated!"
-    if request.method == "DELETE":
-        remove_book(book_id)
-        response_object["message"] = "Book removed!"
-    return jsonify(response_object)
-
-
 @app.errorhandler(400)
 def user_custom_error(error):
-    # TODO 로그추가
     return jsonify({"message": error.description}), 400
 
 
 @app.errorhandler(404)
 def not_found_error(error):
     # TODO 로그추가
+    app.logger.info("%s failed to log in", user.username)
     return jsonify({"message": "404 Not Page founded"}), 404
 
 
